b4ff928670ca95e648814b4129a7cc347d8c7117
Данаил Захариев <85960984+DanailZahariev@users.noreply.github.com>
2021-09-01 19:19:10 +0300

    77/100

--- src/StacksAndQueuesExercise/SImpleTextEditor.java
+++ src/StacksAndQueuesExercise/SImpleTextEditor.java
@@ -13,16 +13,25 @@ public class SImpleTextEditor {
             String[] tokens = scanner.nextLine().split("\\s+");
             switch (tokens[0]) {
                 case "1":
-
+                    text.append(tokens[1]);
+                    stack.push(text.toString());
                     break;
                 case "2":
-
+                    int count = Integer.parseInt(tokens[1]);
+                    text.setLength(text.length() - count);
+                    stack.push(text.toString());
                     break;
                 case "3":
-
+                    int index = Integer.parseInt(tokens[1]);
+                    System.out.println(text.charAt(index - 1));
                     break;
                 case "4":
-
+                    stack.pop();
+                    if (stack.size() > 0) {
+                        text = new StringBuilder(stack.peek());
+                    } else {
+                        text = null;
+                    }
                     break;
             }
         }
