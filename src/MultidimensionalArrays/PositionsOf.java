cf4c9ccbf283c9781295a44df3f8188360860033
Данаил Захариев <85960984+DanailZahariev@users.noreply.github.com>
2021-09-08 21:15:24 +0300

    update

---
+++ src/MultidimensionalArrays/PositionsOf.java
@@ -0,0 +1,41 @@
+package MultidimensionalArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PositionsOf {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] rowsAndCols = readArray(scanner.nextLine());
+        int rows = rowsAndCols[0];
+        int cols = rowsAndCols[1];
+        int[][] matrix = readMatrix(rows, cols, scanner);
+        int searchNumber = Integer.parseInt(scanner.nextLine());
+        boolean isFound = false;
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (matrix[row][col] == searchNumber) {
+                    System.out.println(row + " " + col);
+                    isFound = true;
+                }
+            }
+        }
+        if (!isFound) {
+            System.out.println("not found");
+        }
+    }
+
+
+    public static int[][] readMatrix(int rows, int cols, Scanner scanner) {
+        int[][] matrix = new int[rows][cols];
+        for (int row = 0; row < rows; row++) {
+            matrix[row] = readArray(scanner.nextLine());
+        }
+        return matrix;
+    }
+
+    public static int[] readArray(String elements) {
+        return Arrays.stream(elements.split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+    }
+}
