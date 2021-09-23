a59475728f4bf7f23f847a03fdb6d1e045f409b0
Данаил Захариев <85960984+DanailZahariev@users.noreply.github.com>
2021-09-23 19:57:48 +0300

    update

--- src/SetsAndMapsExercise/CountSymbols.java
+++ src/SetsAndMapsExercise/CountSymbols.java
@@ -1,9 +1,23 @@
 package SetsAndMapsExercise;
 
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class CountSymbols {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Map<Character, Integer> countSymbol = new TreeMap<>();
+        for (int i = 0; i < input.length(); i++) {
+            char currentSymbol = input.charAt(i);
+            if (countSymbol.containsKey(currentSymbol)) {
+                int currentCount = countSymbol.get(currentSymbol);
+                countSymbol.put(currentSymbol, currentCount + 1);
+            } else {
+                countSymbol.put(currentSymbol, 1);
+            }
+        }
+        countSymbol.forEach((key1, value) -> System.out.println(key1 + ": " + value + " time/s"));
     }
 }
