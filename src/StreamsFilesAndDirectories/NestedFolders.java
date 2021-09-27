0c30806d2f42f9c80edb71a3a12c2d9533b9d266
Данаил Захариев <85960984+DanailZahariev@users.noreply.github.com>
2021-09-27 17:59:45 +0300

    update

--- src/StreamsFilesAndDirectories/NestedFolders.java
+++ src/StreamsFilesAndDirectories/NestedFolders.java
@@ -8,41 +8,44 @@ import java.util.Deque;
 
 public class NestedFolders {
     public static void main(String[] args) {
-        String path = "/Users/danailzahariev/Desktop/Java Advanced/Streams, Files and Directories/04. Java-Advanced-Files-and-Streams-Lab-Resources/Files-and-Streams";
-        File root = new File(path);
-        Deque<File> dirs = new ArrayDeque<>();
-        dirs.offer(root);
-        int count = 0;
-        while (!dirs.isEmpty()) {
-            File current = dirs.poll();
-            File[] nestedFiles = current.listFiles();
-            for (File nestedFile : nestedFiles)
-                if (nestedFile.isDirectory())
-                    dirs.offer(nestedFile);
-            count++;
-            System.out.println(current.getName());
+//        String path = "/Users/danailzahariev/Desktop/Java Advanced/Streams, Files and Directories/04. Java-Advanced-Files-and-Streams-Lab-Resources/Files-and-Streams";
+//        File root = new File(path);
+//        Deque<File> dirs = new ArrayDeque<>();
+//        dirs.offer(root);
+//        int count = 0;
+//        while (!dirs.isEmpty()) {
+//            File current = dirs.poll();
+//            File[] nestedFiles = current.listFiles();
+//            for (File nestedFile : nestedFiles)
+//                if (nestedFile.isDirectory())
+//                    dirs.offer(nestedFile);
+//            count++;
+//            System.out.println(current.getName());
+//        }
+//        System.out.println(count + " folders");
+//    }
+//}
+//
+        Path path = Paths.get("/Users/danailzahariev/Desktop/Java Advanced/Streams, Files and Directories/04. Java-Advanced-Files-and-Streams-Lab-Resources/Files-and-Streams");
+        File root = path.toFile();
+        Deque<File> fileDeque = new ArrayDeque<>();
+        fileDeque.offer(root);
+        while (!fileDeque.isEmpty()) {
+            File file = fileDeque.poll();
+            System.out.println(file.getName());
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File f : files) {
+                    if (f.isDirectory()) {
+                        fileDeque.offer(f);
+                    } else {
+                        System.out.println(f.getName());
+                    }
+                }
+            }
         }
-        System.out.println(count + " folders");
     }
 }
-
-//        Path path = Paths.get("/Users/danailzahariev/Desktop/Java Advanced/Streams, Files and Directories/04. Java-Advanced-Files-and-Streams-Lab-Resources/Files-and-Streams");
-//        File root = path.toFile();
-//        Deque<File> fileDeque = new ArrayDeque<>();
-//        fileDeque.offer(root);
-//        while (!fileDeque.isEmpty()) {
-//            File file = fileDeque.poll();
-//            System.out.println(file.getName());
-//            File[] files = file.listFiles();
-//            if (files != null) {
-//                for (File f : files) {
-//                    if (f.isDirectory()) {
-//                        fileDeque.offer(f);
-//                    } else {
-//                        System.out.println(f.getName());
-//                    }
-//                }
-
 //        dfs(root);
 //    }
 //
